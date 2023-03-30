6d0938fe5ca2716f44e0305b8aad026118a1c11b
affanali2k3 <affanalikhanxx@gmail.com>
2023-03-31 00:49:38 +0500

    Home Screen UI Added

--- revpay/lib/Home_Page.dart
+++ revpay/lib/Home_Page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:revpay/widgets/bottomappbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const CustomBottomappbar(),
       body: SafeArea(
-          minimum: EdgeInsets.all(20),
+          minimum: const EdgeInsets.all(20),
           child: Column(
             children: [
               const SizedBox(
@@ -22,111 +25,258 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: const [
                     Text(
-                      'RevPAY',
+                      'My Account',
                       style:
                           TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    Icon(Icons.notifications)
+                    Icon(
+                      Icons.notifications,
+                    )
                   ]),
               const SizedBox(
                 height: 20,
               ),
               Container(
                 decoration: BoxDecoration(
-                    color: Colors.blue,
+                    // color: Color.fromARGB(255, 37, 24, 215),
+                    color: const Color.fromARGB(255, 47, 33, 243),
+                    // color: Color(0xFF52AA8A),
                     borderRadius: BorderRadius.circular(20)),
                 width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                height: 200,
+                padding: const EdgeInsets.all(20),
+                height: 203,
                 child: Column(
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(children: [
-                          ImageIcon(AssetImage('credit-card')),
-                          Text('14301-5273020-1')
+                        Row(children: const [
+                          SizedBox(
+                              height: 40,
+                              width: 40,
+                              child: Image(
+                                  image: AssetImage('assets/images/cc.png'))),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            '**** 2236',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          )
                         ]),
-                        Text('22/04'),
+                        const Text(
+                          '22/04',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
                       ],
                     ),
-                    SizedBox(
-                      height: 20,
+                    const SizedBox(
+                      height: 40,
                     ),
-                    SizedBox(
+                    const SizedBox(
                       width: double.infinity,
-                      child: Text('Balance'),
+                      child: Text(
+                        'Balance',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
                     ),
-                    SizedBox(width: double.infinity, child: Text('\$ 5300.00')),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          '\$ 5300.00',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        )),
                     Container(
                       width: double.infinity,
                       alignment: Alignment.centerRight,
-                      child: Text('More details'),
+                      child: const Text(
+                        'More details',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            decoration: TextDecoration.underline),
+                      ),
                     )
                   ],
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: Colors.green),
-                    child: Icon(Icons.send),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: Colors.green),
-                    child: Icon(Icons.send),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: Colors.green),
-                    child: Icon(Icons.send),
-                  ),
+                  Column(children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: const Offset(0, 3),
+                                blurRadius: 5,
+                                color: Colors.grey.withOpacity(0.5))
+                          ],
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.white),
+                      child: const Icon(
+                        Icons.send,
+                        size: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Send')
+                  ]),
+                  Column(children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: const Offset(0, 3),
+                                blurRadius: 5,
+                                color: Colors.grey.withOpacity(0.5))
+                          ],
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.white),
+                      child: const Icon(
+                        Icons.receipt,
+                        size: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Recieve')
+                  ]),
+                  Column(children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: const Offset(0, 3),
+                                blurRadius: 5,
+                                color: Colors.grey.withOpacity(0.5))
+                          ],
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.white),
+                      child: const Icon(
+                        Icons.money,
+                        size: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Load')
+                  ]),
+                  Column(children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: const Offset(0, 3),
+                                blurRadius: 5,
+                                color: Colors.grey.withOpacity(0.5))
+                          ],
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.white),
+                      child: const Icon(
+                        Icons.more,
+                        size: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('More')
+                  ]),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
               Container(
+                  padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 5, color: Colors.grey.withOpacity(0.5))
+                      ],
                       borderRadius: BorderRadius.circular(20),
                       color: Colors.white),
                   child: Column(children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [Text('Recent Transactions'), Text('View All')],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.receipt),
-                        Column(children: [
-                          Text('Recent Transactions'),
-                          Text(DateTime.now().toString())
-                        ]),
-                        Text('\$5')
+                      children: const [
+                        Text(
+                          'Recent Transactions',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          'View All',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        )
                       ],
                     ),
+                    const SizedBox(
+                      height: 30,
+                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(Icons.receipt),
+                        const Icon(
+                          Icons.outbond,
+                          size: 32,
+                        ),
                         Column(children: [
-                          Text('Recent Transactions'),
-                          Text(DateTime.now().toString())
+                          const Text(
+                            'Affan Ali',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                              '${DateFormat.jm().format(DateTime.now())} ${DateFormat.yMMMd().format(DateTime.now())}')
                         ]),
-                        Text('\$5')
+                        const Text(
+                          '\$5',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Icon(
+                          Icons.outbond,
+                          size: 32,
+                        ),
+                        Column(children: [
+                          const Text(
+                            'Affan Ali',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                              '${DateFormat.jm().format(DateTime.now())} ${DateFormat.yMMMd().format(DateTime.now())}')
+                        ]),
+                        const Text(
+                          '\$5',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 20),
+                        )
                       ],
                     ),
                   ]))
--- revpay/lib/main.dart
+++ revpay/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:revpay/Home_Page.dart';
+import 'package:revpay/home_page.dart';
 
 void main() {
   runApp(const MyApp());
